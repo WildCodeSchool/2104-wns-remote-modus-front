2052e362727c45a1d3988c95a3bbdaf9186caa70
Alexis Roger <a.roger37@gmail.com>
2021-04-28 11:24:35 +0200

    Wysiwyg installed and rendered, no style and toolkit-bar yet

--- src/routes/askinghelpform/AskingHelpForm.tsx
+++ src/routes/askinghelpform/AskingHelpForm.tsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
+import { createEditor, BaseEditor, Descendant } from "slate";
+import { Slate, Editable, withReact, ReactEditor } from "slate-react";
 
-const AskingHelpForm = (): JSX.Element => {
-  // formulaire de demande d'aide avec le wysiwyg
-  return <>AskingHelpForm</>;
+type CustomText = { text: string };
+type CustomElement = { type: "paragraph"; children: CustomText[] };
+
+declare module "slate" {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor;
+    Element: CustomElement;
+    Text: CustomText;
+  }
+}
+
+const AskingHelpForm: React.FC = () => {
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const [value, setValue] = useState<Descendant[]>([
+    { type: "paragraph", children: [{ text: "" }] },
+  ]);
+
+  return (
+    <>
+      <Slate
+        editor={editor}
+        value={value}
+        onChange={(newValue) => setValue(newValue)}
+      >
+        <Editable
+          onKeyDown={(event) => {
+            if (event.key === "&") {
+              event.preventDefault();
+              editor.insertText("and");
+            }
+          }}
+        />
+      </Slate>
+      <p>toto</p>
+    </>
+  );
 };
 
 export default AskingHelpForm;
