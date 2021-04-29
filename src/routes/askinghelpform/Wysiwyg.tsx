7a765c5075a283aa0c9159405e471ff305d588dd
ZoldekSama <zoldeksama35@gmail.com>
2021-04-29 16:04:00 +0200

    Add buttons into the toolbar to edit the styles and add a code-block

--- src/routes/askinghelpform/Wysiwyg.tsx
+++ src/routes/askinghelpform/Wysiwyg.tsx
@@ -6,11 +6,30 @@ import "react-quill/dist/quill.snow.css";
 export default function Wysiwyg(): JSX.Element {
   const [userInput, setUserInput] = useState("");
 
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      [{ size: ["small", false, "large", "huge"] }],
+      [{ header: 1 }, { header: 2 }],
+      ["bold", "italic", "underline", "blockquote"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ script: "sub" }, { script: "super" }],
+      [{ indent: "-1" }, { indent: "+1" }],
+      ["link"],
+      ["clean"],
+      ["code-block"],
+    ],
+    clipboard: {
+      matchVisual: false,
+    },
+  };
+
   return (
     <ReactQuill
       value={userInput}
       placeholder="Please insert your text here..."
       onChange={setUserInput}
+      modules={modules}
     />
   );
 }
