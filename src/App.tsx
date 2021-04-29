646cc29b02585952214bb6e6692ea40de516d598
dcirlan <kirlan@hotmail.fr>
2021-04-29 16:18:34 +0200

    style : added some style to navbar + modified texts on all pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import AskingHelpForm from "./routes/askinghelpform/AskingHelpForm";
 import Home from "./routes/home/Home";
 import Navbar from "./routes/navbar/Navbar";
+import PostsList from "./routes/postslist/PostsList";
 
 const App = (): JSX.Element => {
   return (
@@ -15,6 +16,9 @@ const App = (): JSX.Element => {
         </Route>
         <Route exact path="/ask-help">
           <AskingHelpForm />
+        </Route>
+        <Route exact path="/posts-list">
+          <PostsList />
         </Route>
       </Switch>
     </Router>
--- src/routes/navbar/Navbar.tsx
+++ src/routes/navbar/Navbar.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "../../css/styles.css";
+import "./navbar.css";
 
 const Navbar = (): JSX.Element => {
   return (
-    <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/ask-help">AskingHelpForm</NavLink>
+    <div className="topnav">
+      <div>
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+        <NavLink to="/ask-help">AskingHelpForm</NavLink>
+        <NavLink to="/posts-list">PostsList</NavLink>
+      </div>
+      <div className="welcomemessage">
+        <div className="topnav message">Welcome user@mail.com</div>
+        <img src="user-solid.svg" alt="profileIcon" className="profileIcon" />
+      </div>
     </div>
   );
 };
