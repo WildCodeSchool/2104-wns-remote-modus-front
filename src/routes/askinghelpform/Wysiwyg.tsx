8adfc30cae382a5fd9c40d4ad54f5d9311afc80a
ZoldekSama <zoldeksama35@gmail.com>
2021-04-29 16:01:46 +0200

    Wysiwyg is functionnal, recover  data entered by the user inside

--- src/routes/askinghelpform/Wysiwyg.tsx
+++ src/routes/askinghelpform/Wysiwyg.tsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
 
-export default function Wysiwyg() {
-  return <div>Wysiwyg</div>;
+import "react-quill/dist/quill.snow.css";
+
+export default function Wysiwyg(): JSX.Element {
+  const [userInput, setUserInput] = useState("");
+
+  return (
+    <ReactQuill
+      value={userInput}
+      placeholder="Please insert your text here..."
+      onChange={setUserInput}
+    />
+  );
 }
