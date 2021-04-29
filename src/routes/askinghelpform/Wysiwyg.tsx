7e7207e5503cf7e3e500ed02bf69c0e0999efa6d
ZoldekSama <zoldeksama35@gmail.com>
2021-04-29 11:30:46 +0200

    Slate removed, go on Quill solution

--- src/routes/askinghelpform/Wysiwyg.tsx
+++ src/routes/askinghelpform/Wysiwyg.tsx
@@ -1,43 +1,5 @@
-import React, { useMemo, useState } from "react";
-import { createEditor, BaseEditor, Descendant } from "slate";
-import { Slate, Editable, withReact, ReactEditor } from "slate-react";
+import React from "react";
 
-type CustomText = { text: string };
-type CustomElement = { type: "paragraph"; children: CustomText[] };
-
-declare module "slate" {
-  interface CustomTypes {
-    Editor: BaseEditor & ReactEditor;
-    Element: CustomElement;
-    Text: CustomText;
-  }
+export default function Wysiwyg() {
+  return <div>Wysiwyg</div>;
 }
-
-const Wysiwyg: React.FC = () => {
-  const editor = useMemo(() => withReact(createEditor()), []);
-  const [value, setValue] = useState<Descendant[]>([
-    { type: "paragraph", children: [{ text: "" }] },
-  ]);
-
-  return (
-    <div className="bg-red-500 h-40">
-      <Slate
-        editor={editor}
-        value={value}
-        onChange={(newValue) => setValue(newValue)}
-      >
-        <Editable
-          onKeyDown={(event) => {
-            if (event.key === "&") {
-              event.preventDefault();
-              editor.insertText("and");
-            }
-          }}
-        />
-      </Slate>
-      <p>toto</p>
-    </div>
-  );
-};
-
-export default Wysiwyg;
