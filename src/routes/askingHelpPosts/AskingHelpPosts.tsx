a3773210ef57e2ea195679bb56de193e8d643ec1
dcirlan <kirlan@hotmail.fr>
2021-04-30 11:06:47 +0200

    Create an array of fake data, we can use it

--- src/routes/askingHelpPosts/AskingHelpPosts.tsx
+++ src/routes/askingHelpPosts/AskingHelpPosts.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "../../css/styles.css";
+import fakeDataPosts from "./fakeDataPosts";
 
 const AskingHelpPosts = (): JSX.Element => {
+  console.log(fakeDataPosts);
+
   return (
     <>
       <h1>HELLO </h1>
+      <div className="container">
+        <p className="test">Hello</p>
+      </div>
     </>
   );
 };
---
+++ src/routes/askingHelpPosts/fakeDataPosts.ts
@@ -0,0 +1,22 @@
+const fakeDataPosts = [
+  {
+    title: "Problème array.map",
+    techno: "JavaScript",
+    summary:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    title: "Fonctionnement des imports",
+    techno: "React",
+    summary:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    title: "useEffect",
+    techno: "React",
+    summary:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+];
+
+export default fakeDataPosts;
