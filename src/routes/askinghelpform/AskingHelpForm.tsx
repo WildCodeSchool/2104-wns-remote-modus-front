b2ba7f79ea6988f73ae82a1991df44d960a41257
Alice Hamel <alice.hamel@hiventive.com>
2021-05-26 16:31:13 +0200

    started

--- src/routes/askinghelpform/AskingHelpForm.tsx
+++ src/routes/askinghelpform/AskingHelpForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Select from "react-select";
+import { Button, Flex, Box } from "@chakra-ui/react";
 import Wysiwyg from "./Wysiwyg";
 import Skill from "../../models/Skill";
 
@@ -32,78 +33,87 @@ const AskingHelpForm: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <h3>
-        Ici vous pouvez remplir une demande d&apos;aide sur un sujet spécifique
-      </h3>
-      <form
-        className="border-solid border-yellow-500 border-4 w-3/5 p-8"
-        onSubmit={(e) => onSubmitLog(e)}
+    <Flex justifyContent="center" alignItems="center">
+      <Box
+        marginTop="10"
+        borderWidth="1px"
+        borderColor="orange"
+        padding="16"
+        borderRadius="lg"
+        overflow="hidden"
       >
-        <div>
-          <label htmlFor="title">
-            Titre de la demande :
-            <input
-              className="border-2 border-solid ml-4"
-              type="text"
-              name="title"
-              value={titleHelp}
-              placeholder="Titre..."
-              onChange={(e) => setTitleHelp(e.target.value)}
-              required
-            />
-          </label>
-          <p className="text-center mb-4 mt-4">Technologie(s) concernée(s) :</p>
-          <div className="flex justify-between">
-            <div className="flex w-full items-center">
-              <Select
-                className="w-3/5"
+        <h3>
+          Ici vous pouvez remplir une demande d&apos;aide sur un sujet
+          spécifique
+        </h3>
+        <form
+          className="border-solid border-yellow-500 border-4 w-3/5 p-8"
+          onSubmit={(e) => onSubmitLog(e)}
+        >
+          <div>
+            <label htmlFor="title">
+              Titre de la demande :
+              <input
+                className="border-2 border-solid ml-4"
+                type="text"
+                name="title"
+                value={titleHelp}
+                placeholder="Titre..."
+                onChange={(e) => setTitleHelp(e.target.value)}
                 required
-                options={options}
-                onChange={(result: any) => {
-                  if (result) {
-                    setSkill(result.value);
-                  }
-                }}
               />
-              <button
-                className="ml-4 border-2 rounded-full h-8 w-8 flex items-center justify-center"
-                type="button"
-                disabled={skills.includes(skill)}
-                onClick={() => {
-                  setSkills([...skills, skill]);
-                }}
-              >
-                +
-              </button>
+            </label>
+            <p className="text-center mb-4 mt-4">
+              Technologie(s) concernée(s) :
+            </p>
+            <div className="flex justify-between">
+              <div className="flex w-full items-center">
+                <Select
+                  className="w-3/5"
+                  required
+                  options={options}
+                  onChange={(result: any) => {
+                    if (result) {
+                      setSkill(result.value);
+                    }
+                  }}
+                />
+                <Button
+                  size="xs"
+                  type="button"
+                  disabled={skills.includes(skill)}
+                  onClick={() => {
+                    setSkills([...skills, skill]);
+                  }}
+                >
+                  +
+                </Button>
+              </div>
+            </div>
+            <div className="flex">
+              {skills.map((oneSkill) => (
+                <Skill
+                  title={oneSkill}
+                  onDelete={(title: string) => DeleteSkill(title)}
+                  key={id + ((Math.random() * 10) / Math.random()) * 15}
+                />
+              ))}
             </div>
           </div>
-          <div className="flex">
-            {skills.map((oneSkill) => (
-              <Skill
-                title={oneSkill}
-                onDelete={(title: string) => DeleteSkill(title)}
-                key={id + ((Math.random() * 10) / Math.random()) * 15}
-              />
-            ))}
+          <div className="mt-12">
+            <p className="mb-4 mt-4">
+              Contexte et description du probleme (wysiwyg):
+            </p>
+            <Wysiwyg userInput={userInput} setUserInput={setUserInput} />
+            <Flex justifyContent="end">
+              <Button type="submit" colorScheme="orange" size="sm">
+                Ajouter
+              </Button>
+            </Flex>
           </div>
-        </div>
-        <div className="mt-12">
-          <p className="mb-4 mt-4">
-            Contexte et description du probleme (wysiwyg):
-          </p>
-          <Wysiwyg userInput={userInput} setUserInput={setUserInput} />
-          <div className="flex justify-end mt-4">
-            <button
-              className="border-2 p-1 flex items-center justify-center"
-              type="submit"
-            >
-              Ajouter
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </Box>
+    </Flex>
   );
 };
 
